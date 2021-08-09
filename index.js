9f3c5b132ce0bf507dfb7563739bef152ca82724
DannyJS-933 <danny.933@hotmail.com>
2021-08-09 14:31:40 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const input = document.getElementById('fruit');
+const button = document.getElementById('submit');
+const targetNode = document.getElementById('myList');
+
+const config = {
+
+attributes: true,
+childList: true,
+subtree: true,
+
+};
+
+button.addEventListener('click', add);
+
+    function add(){
+
+        const li = document.createElement('li');
+        const text = input.value;
+        li.appendChild(document.createTextNode(text));
+        targetNode.appendChild(li);
+
+    };
+
+
+//mutation observer 
+
+
+const callback = (mutationList, observer) => {
+    for(const mutation of mutationList){
+        if(mutation.type === 'childList'){
+            console.log('Child added or removed')
+        } else if (mutation.type === 'attributes'){
+            console.log(`The ${mutation.attributeName} attribute was modified`)
+        };
+    };
+};
+
+const observer = new MutationObserver(callback);//check the function called callback, it checks the changes
+
+observer.observe(targetNode, config);
+//The observer is reviewing the targetNode where myList is located
+//observer.disconnect();
